1c795b606c95b3c600788ddec691a2a4bd16dcd6
Hyro0823 <estudiante@GTACAD.USC>
2023-05-13 16:00:36 -0500

    primer commit

---
+++ ParcialTresA/src/ParcialA/NavesRoboticas.java
@@ -0,0 +1,27 @@
+package ParcialA;
+
+public class NavesRoboticas {
+
+	private String EEUU;
+	private String Rusia;
+	private String Europa;
+	
+	public NavesRoboticas (String EEUU, String Rusia, String Europa){
+		this.EEUU= EEUU;
+		this.Rusia= Rusia;
+		this.Europa= Europa;
+	
+}
+	public String getEEUU(){
+		return EEUU;
+		
+	}
+	public String getRusia(){
+		return Rusia;
+		
+	}
+	public String getEuropa(){
+		return Europa;
+		
+	}
+}
